c242f4585bd0002de02be473b8647f8666fcaa7a
Aaquiff <aaquiff@gmail.com>
2018-09-12 18:43:07 +0530

    Removed uneccessary code

--- src/main/java/com/aaralk/synchronizertokenpattern/Login.java
+++ src/main/java/com/aaralk/synchronizertokenpattern/Login.java
@@ -22,54 +22,6 @@ import javax.servlet.http.HttpServletResponse;
 public class Login extends HttpServlet {
 
     /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        try {
-            String username = request.getParameter("username");
-            String password = request.getParameter("password");
-
-            if (username == "aaralk" && password == "aaralk") {
-
-                Cookie c1 = new Cookie("KSESSIONID", "anything");
-                c1.setMaxAge(60 * 60);
-                c1.setSecure(true);
-
-                response.addHeader("Set-Cookie", "name=value; HttpOnly");
-
-                response.addCookie(c1);
-            } else {
-            }
-
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        } finally {
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
      * Handles the HTTP <code>POST</code> method.
      *
      * @param request servlet request
